30191cff1830e0aaf248d0a950222f2ab9cd3248
AlenaNik <alena.nik.digital@gmail.com>
2019-04-15 15:33:40 +0200

    doing exercise

---
+++ src/Lamp.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Lamp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            on: false
+        };
+    }
+    render() {
+        const light = this.props.on ? 'on': 'off';
+        return (
+            <div className="Lamp">
+                <button onClick={() => alert("Button clicked")} className={light}>{light.toUpperCase()}</button>
+                <figure className={light} />
+            </div>
+        );
+    }
+}
+export default Lamp;
